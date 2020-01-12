f480b0788c04ee79fc0d056ea41b0142b4279265
root <root@localhost.localdomain>
2020-01-12 19:00:08 +0530

    Updates

---
+++ precache-manifest.97828a5035b06fd99f3c6cfb05a18962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047828faec378777e2cca7fdf4a2a65a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13be3bb2bae00b2197db",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b788cb23352f2b313832",
+    "url": "/static/js/2.690bd166.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.690bd166.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13be3bb2bae00b2197db",
+    "url": "/static/js/main.470df86f.chunk.js"
+  },
+  {
+    "revision": "e8f9bd665d3ab4aeb0e6",
+    "url": "/static/js/runtime-main.927a6b1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
